f8601b7b7472cdfa36912efb6d217d3f5dc6401e
Drew & enrique <drew@finstrom.us>
2015-05-06 11:14:09 -0700

    return a list of .all saved books

--- lib/book.rb
+++ lib/book.rb
@@ -7,11 +7,21 @@ class Book
   end
 
   define_method(:save) do
-
+    sql = "INSERT INTO book (title, author_id)
+    VALUES ('#{@title}', #{@author_id})"
+    DB.exec(sql)
   end
 
   define_singleton_method(:all) do
-    []
+    books = []
+    sql = "SELECT * FROM book"
+    results = DB.exec(sql)
+    results.each do |result|
+      title = result.fetch('title')
+      author_id = result.fetch('author_id')
+      books.push(Book.new({ :title => title, :author_id => author_id}))
+    end
+    books
   end
 
 end
