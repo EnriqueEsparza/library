bf94cb592dd2859c531b335fde922d7827fb2654
Drew & enrique <drew@finstrom.us>
2015-05-06 10:53:43 -0700

    pass first spec

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,9 @@
 class Book
-  attr_reader()
+  attr_reader(:title, :author_id)
 
   define_method(:initialize) do |attributes|
-
+    @title = attributes.fetch(:title)
+    @author_id = attributes.fetch(:author_id)
   end
 
   define_method(:save) do
--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,11 +1,11 @@
 require('spec_helper')
 
 describe(Book) do
-  describe('.all') do
-    it('returns all created books') do
+  describe('#title') do
+    it('returns title of book object') do
       new_book = Book.new({ :title => 'Intro to Ruby', :author_id => 1 })
-      new_book.save()
-      expect(Book.all().length).to(eq(1))
+      expect(new_book.title()).to(eq('Intro to Ruby'))
+      expect(new_book.author_id()).to(eq(1))
     end
   end
 end
