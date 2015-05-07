bf736396a4da8e8e0dbb23e45e419526d156886e
Guest User <student@epicodus.com>
2015-05-07 12:03:49 -0700

    unit testing and method to return list of overdue books

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,5 @@
+require('date')
+
 class Checkout
   attr_reader(:patron_id, :book_copy_id, :on_date)
 
@@ -30,6 +32,23 @@ class Checkout
     VALUES ('#{@patron_id}', #{@book_copy_id}, '#{@on_date}') RETURNING id;"
     result = DB.exec(sql)
     @id = result.first().fetch("id").to_i()
+  end
+
+  define_singleton_method(:all_overdue) do
+    today = Date.today
+    overdue = (today - 20).to_s
+    overdue_books = []
+    sql = "SELECT * FROM checkedout WHERE on_date < '#{overdue}'"
+    results = DB.exec(sql)
+    results.each do |result|
+      id = result.fetch('id').to_i()
+      book_copy_id = result.fetch('book_copy_id').to_i()
+      on_date = result.fetch('on_date')
+      patron_id = result.fetch('patron_id')
+      checkout = Checkout.new({ :on_date => on_date, :id => id, :patron_id => patron_id, :book_copy_id => book_copy_id })
+      overdue_books.push(checkout)
+    end
+    overdue_books
   end
 
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -9,4 +9,16 @@ describe(Checkout) do
       expect(Checkout.find_by_patron_id(3).count()).to(eq(2))
     end
   end
+
+  describe('.all_overdue') do
+    it('returns a list of books that were due back prior to today (20 days ago)') do
+      checkout1 = Checkout.new({ :patron_id => 3, :on_date => '2015-05-7', :book_copy_id => 4})
+      checkout1.save()
+      checkout1 = Checkout.new({ :patron_id => 3, :on_date => '2014-07-11', :book_copy_id => 9})
+      checkout1.save()
+      checkout1 = Checkout.new({ :patron_id => 3, :on_date => '2015-04-15', :book_copy_id => 9})
+      checkout1.save()
+      expect(Checkout.all_overdue().count()).to(eq(2))
+    end
+  end
 end
