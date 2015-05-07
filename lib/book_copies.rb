66219c411a311c4e5ca1296c5c18d7b055711588
Guest User <student@epicodus.com>
2015-05-07 10:01:41 -0700

    pass rspec to find ID for book_copies

--- lib/book_copies.rb
+++ lib/book_copies.rb
@@ -2,10 +2,13 @@ class BookCopies
   attr_reader(:id)
 
   define_method(:initialize) do |attributes|
-
+    @book_id = attributes.fetch(:book_id)
   end
 
   define_method(:save) do
+    sql = "INSERT INTO book_copies (book_id) VALUES (#{@book_id}) RETURNING id;"
+    result = DB.exec(sql)
+    @id = result.first().fetch("id").to_i()
 
   end
 
